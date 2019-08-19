92eb6f5db80704403936143210c9c64f759a9f0a
Fenton Taylor <fentontaylor@gmail.com>
2019-08-18 19:50:02 -0600

    Define 1:many relationship, article:comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 describe Comment, type: :model do
-  it { should belong_to(:article) }
+  describe 'relationships' do
+    it { should belong_to(:article) }
+  end
+
+  describe 'validations' do
+  end
 end
